29b11a20ed6c878491a4ebc6c8ce9eb4523c14e1
Sebastian <brokecarnotauro@gmail.com>
2024-10-16 16:13:58 -0600

    Se agregan puertos udp como destino

--- trafico.py
+++ trafico.py
@@ -1,19 +1,24 @@
 import random
 import logging
-from scapy.all import IP, TCP, send
+from scapy.all import IP, TCP, UDP, send
 
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(message)s')
 
 ip_range = ['10.0.0.1', '10.0.0.2', '10.0.0.3', '10.0.0.4']
-tcp_ports = [80, 443, 21, 22, 23, 25, 69, 109, 110, 119]
+tcp_ports = [20, 21, 22, 23, 25, 80, 110, 143, 443]
+udp_ports = [53, 67, 68, 69, 123, 161, 162, 500]
 
 def generate_random_packet():
     dst_ip = random.choice(ip_range)
     dst_port = random.choice(tcp_ports)
     src_port = random.randint(49152, 65535)
     
-    packet = IP(dst=dst_ip) / TCP(dport=dst_port, sport=src_port)
-    
+    protocol = random.choice(['TCP', 'UDP'])
+
+    if protocol == 'TCP':
+        packet = IP(dst=dst_ip) / TCP(dport=dst_port, sport=src_port)
+    else:
+        packet = IP(dst=dst_ip) / UDP(dport=dst_port, sport=src_port)
     
     logging.info(f'Enviando paquete: {packet.summary()}')
     
