768cf0928ad3d7deb99f6d34b8eae8428b15c1af
Sebastian <brokecarnotauro@gmail.com>
2024-10-16 14:48:40 -0600

    Se crean puertos origen dinamicos en trafico

--- trafico.py
+++ trafico.py
@@ -4,14 +4,15 @@ from scapy.all import IP, TCP, send
 
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(message)s')
 
-ip_range = ['10.0.0.1', '10.0.0.2']
+ip_range = ['10.0.0.1', '10.0.0.2', '10.0.0.3', '10.0.0.4']
 tcp_ports = [80, 443, 21, 22, 23, 25, 69, 109, 110, 119]
 
 def generate_random_packet():
     dst_ip = random.choice(ip_range)
     dst_port = random.choice(tcp_ports)
+    src_port = random.randint(49152, 65535)
     
-    packet = IP(dst=dst_ip) / TCP(dport=dst_port)
+    packet = IP(dst=dst_ip) / TCP(dport=dst_port, sport=src_port)
     
     
     logging.info(f'Enviando paquete: {packet.summary()}')
