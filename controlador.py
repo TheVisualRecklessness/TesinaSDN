d693f6403560ae25dd55a4b77c1b8bce792f8bbb
sebastian <brokecarnotauro@gmail.com>
2024-10-16 00:01:31 -0600

    codigo nuevo

--- controlador.py
+++ controlador.py
@@ -61,6 +61,7 @@ class SimpleSwitch13(app_manager.RyuApp):
         self.mac_to_port.setdefault(dpid, {})
 
         self.logger.info("packet in %s %s %s %s", dpid, src, dst, in_port)
+        #hm
 
         self.mac_to_port[dpid][src] = in_port
 
