57340c1ef27ab50f91bd0d166d596bb8de6dd72d
Sebastian <brokecarnotauro@gmail.com>
2024-10-17 15:29:08 -0600

    Se cambia la manera en la que se llama a la tabla de flujo

--- controlador.py
+++ controlador.py
@@ -2,7 +2,7 @@ from ryu.base import app_manager
 from ryu.controller import ofp_event
 from ryu.controller.handler import MAIN_DISPATCHER, CONFIG_DISPATCHER
 from ryu.controller.handler import set_ev_cls
-from ryu.ofproto import ofproto_v1_3, ofproto_v1_3_parser
+from ryu.ofproto import ofproto_v1_3, ofproto_v1_3_parser, inet
 from ryu.lib.packet import packet, ethernet, ether_types, ipv4, arp, icmp, tcp, udp
 from dataset import malicious_ports, malicious_ips, malicious_macs
 
@@ -139,30 +139,39 @@ class SimpleSwitch13(app_manager.RyuApp):
     @set_ev_cls(ofp_event.EventOFPFlowStatsReply, MAIN_DISPATCHER)
     def flow_stats_reply_handler(self, ev):
         body = ev.msg.body
-
+    
         self.logger.info('datapath         '
                          'in-port  eth-dst           '
-                         'out-port action  packets  bytes  dst-port')
+                         'out-port packets  bytes    '
+                         'src-port dst-port')
         self.logger.info('---------------- '
                          '-------- ----------------- '
-                         '-------- ------- -------- -------- --------')
-        for stat in sorted([flow for flow in body if flow.priority == 1],
-                           key=lambda flow: (flow.match['in_port'],
-                                             flow.match['eth_dst'])):
-            actions = stat.instructions[0].actions
-            for action in actions:
-                if isinstance(action, ofproto_v1_3_parser.OFPActionOutput):
-                    # Extract destination port from the match fields
-                    dst_port = None
-                    if 'tcp_dst' in stat.match:
-                        dst_port = stat.match['tcp_dst']
-                    elif 'udp_dst' in stat.match:
-                        dst_port = stat.match['udp_dst']
-                    
-                    self.logger.info('%016x %8x %17s %8x %7s %8d %8d %8s',
-                                     ev.msg.datapath.id,
-                                     stat.match['in_port'], stat.match['eth_dst'],
-                                     action.port, 'OUTPUT',
-                                     stat.packet_count, stat.byte_count,
-                                     dst_port if dst_port else 'N/A')
-                    
+                         '-------- -------- -------- '
+                         '-------- --------')
+        for stat in sorted([flow for flow in body], key=lambda flow: (flow.match['in_port'], flow.match['eth_dst'])):
+            actions = [str(action) for action in stat.instructions]
+            
+            src_port = dst_port = 'N/A'
+            if 'ipv4_src' in stat.match and 'ipv4_dst' in stat.match:
+                pkt = packet.Packet(stat.match['ipv4_src'])
+                ip_pkt = pkt.get_protocol(ipv4.ipv4)
+                if ip_pkt:
+                    if ip_pkt.proto == inet.IPPROTO_TCP:
+                        tcp_pkt = pkt.get_protocol(tcp.tcp)
+                        if tcp_pkt:
+                            src_port = tcp_pkt.src_port
+                            dst_port = tcp_pkt.dst_port
+                    elif ip_pkt.proto == inet.IPPROTO_UDP:
+                        udp_pkt = pkt.get_protocol(udp.udp)
+                        if udp_pkt:
+                            src_port = udp_pkt.src_port
+                            dst_port = udp_pkt.dst_port
+    
+            self.logger.info('%016x %8x %17s %8x %8d %8d %8s %8s %s',
+                             ev.msg.datapath.id,
+                             stat.match['in_port'],
+                             stat.match['eth_dst'],
+                             stat.instructions[0].actions[0].port if stat.instructions else 'N/A',
+                             stat.packet_count, stat.byte_count,
+                             src_port, dst_port,
+                             ', '.join(actions))
