8aaf428ec1093ecbe6d2821f64dcb0ff4b6a5659
Sebastian <brokecarnotauro@gmail.com>
2024-10-22 16:46:19 -0600

    Se agrega defensa de ataque de escaneo de puertos con 3 puertos

--- scaneo_de_puerto.py
+++ scaneo_de_puerto.py
@@ -5,14 +5,22 @@ from ryu.controller.handler import set_ev_cls
 from ryu.ofproto import ofproto_v1_3
 from ryu.lib.packet import packet
 from ryu.lib.packet import ethernet
+from ryu.lib.packet import ipv4
+from ryu.lib.packet import tcp, udp
 from ryu.lib.packet import ether_types
+import time
 
 class SimpleSwitch13(app_manager.RyuApp):
     OFP_VERSIONS = [ofproto_v1_3.OFP_VERSION]
 
+    # Threshold for detecting port scans
+    PORT_SCAN_THRESHOLD = 3  # Unique ports
+    TIME_WINDOW = 10  # Time window in seconds to detect scanning
+
     def __init__(self, *args, **kwargs):
         super(SimpleSwitch13, self).__init__(*args, **kwargs)
         self.mac_to_port = {}
+        self.scan_tracker = {}  # To track source IP and accessed ports with timestamps
 
     @set_ev_cls(ofp_event.EventOFPSwitchFeatures, CONFIG_DISPATCHER)
     def switch_features_handler(self, ev):
@@ -39,6 +47,34 @@ class SimpleSwitch13(app_manager.RyuApp):
                                     match=match, instructions=inst)
         datapath.send_msg(mod)
 
+    def detect_port_scan(self, src_ip, dst_port):
+        current_time = time.time()
+
+        # Initialize tracking for new source IPs
+        if src_ip not in self.scan_tracker:
+            self.scan_tracker[src_ip] = {'ports': set(), 'timestamps': []}
+
+        # Track accessed port and timestamp
+        self.scan_tracker[src_ip]['ports'].add(dst_port)
+        self.scan_tracker[src_ip]['timestamps'].append(current_time)
+
+        # Remove old timestamps (outside the time window)
+        self.scan_tracker[src_ip]['timestamps'] = [t for t in self.scan_tracker[src_ip]['timestamps']
+                                                   if current_time - t <= self.TIME_WINDOW]
+
+        # Check if the number of unique ports exceeds the threshold
+        if len(self.scan_tracker[src_ip]['ports']) > self.PORT_SCAN_THRESHOLD:
+            return True
+        return False
+
+    def block_ip(self, datapath, src_ip):
+        """ Block traffic from a source IP by installing a drop rule """
+        parser = datapath.ofproto_parser
+        match = parser.OFPMatch(ipv4_src=src_ip)
+        actions = []  # No actions means drop
+        self.add_flow(datapath, 10, match, actions)
+        self.logger.info(f"Port scan detected from {src_ip}. Blocking IP.")
+
     @set_ev_cls(ofp_event.EventOFPPacketIn, MAIN_DISPATCHER)
     def _packet_in_handler(self, ev):
         msg = ev.msg
@@ -50,27 +86,48 @@ class SimpleSwitch13(app_manager.RyuApp):
         pkt = packet.Packet(msg.data)
         eth = pkt.get_protocols(ethernet.ethernet)[0]
 
-        dst = eth.dst
-        src = eth.src
-
-        dpid = datapath.id
-        self.mac_to_port.setdefault(dpid, {})
-
-        self.logger.info("packet in %s %s %s %s", dpid, src, dst, in_port)
+        if eth.ethertype == ether_types.ETH_TYPE_IPV6:
+            return  # Ignore IPv6 packets
 
         # Learn a mac address to avoid FLOOD next time.
-        self.mac_to_port[dpid][src] = in_port
+        dpid = datapath.id
+        src_mac = eth.src
+        dst_mac = eth.dst
+        self.mac_to_port.setdefault(dpid, {})
 
-        if dst in self.mac_to_port[dpid]:
-            out_port = self.mac_to_port[dpid][dst]
+        self.mac_to_port[dpid][src_mac] = in_port
+
+        if dst_mac in self.mac_to_port[dpid]:
+            out_port = self.mac_to_port[dpid][dst_mac]
         else:
             out_port = ofproto.OFPP_FLOOD
 
         actions = [parser.OFPActionOutput(out_port)]
 
+        # If packet is IPv4, check for port scanning
+        ipv4_pkt = pkt.get_protocol(ipv4.ipv4)
+        if ipv4_pkt:
+            src_ip = ipv4_pkt.src
+            dst_ip = ipv4_pkt.dst
+
+            # Check for TCP or UDP protocol
+            tcp_pkt = pkt.get_protocol(tcp.tcp)
+            udp_pkt = pkt.get_protocol(udp.udp)
+
+            if tcp_pkt:
+                dst_port = tcp_pkt.dst_port
+                if self.detect_port_scan(src_ip, dst_port):
+                    self.block_ip(datapath, src_ip)
+                    return
+            elif udp_pkt:
+                dst_port = udp_pkt.dst_port
+                if self.detect_port_scan(src_ip, dst_port):
+                    self.block_ip(datapath, src_ip)
+                    return
+
         # Install a flow to avoid packet_in next time
         if out_port != ofproto.OFPP_FLOOD:
-            match = parser.OFPMatch(in_port=in_port, eth_dst=dst)
+            match = parser.OFPMatch(in_port=in_port, eth_dst=dst_mac)
             if msg.buffer_id != ofproto.OFP_NO_BUFFER:
                 self.add_flow(datapath, 1, match, actions, msg.buffer_id)
                 return
